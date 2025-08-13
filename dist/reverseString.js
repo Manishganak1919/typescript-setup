98badc21ae48d3c8e0d3ae293d1c97cbbc31718e
manish typescript <tejnarayan1919@gmail.com>
2025-08-13 13:14:06 +0530

    reverse the string

--- dist/reverseString.js
+++ dist/reverseString.js
@@ -1,2 +1,11 @@
 "use strict";
 const str = "Manish";
+function ReverseStr(str) {
+    const n = str.length; // n = 6
+    let ans = "";
+    for (let i = n - 1; i >= 0; i--) {
+        ans = ans + str[i];
+    }
+    return ans;
+}
+console.log(ReverseStr(str));
--- src/reverseString.ts
+++ src/reverseString.ts
@@ -1,2 +1,11 @@
-const str:string = "Manish";
+const str: string = "Manish";
 
+function ReverseStr(str: string): string {
+  const n = str.length; // n = 6
+  let ans = "";
+  for (let i = n-1; i >= 0; i--) {
+    ans = ans + str[i];
+  }
+  return ans;
+}
+console.log(ReverseStr(str));
