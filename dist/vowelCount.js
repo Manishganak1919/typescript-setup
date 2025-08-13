c864649ae30a6b2309a2f687d18c704b9e4979fd
manish typescript <tejnarayan1919@gmail.com>
2025-08-13 13:33:20 +0530

    vowel count in string

---
+++ dist/vowelCount.js
@@ -0,0 +1,13 @@
+"use strict";
+function CountVowelInstring(str) {
+    const n = str.length;
+    const vowel = ["a", "e", "i", "o", "u"];
+    let cnt = 0;
+    for (let i = 0; i < n; i++) {
+        if (vowel.includes(str[i].toLocaleLowerCase())) {
+            cnt++;
+        }
+    }
+    return cnt;
+}
+console.log(CountVowelInstring("Hello World"));
