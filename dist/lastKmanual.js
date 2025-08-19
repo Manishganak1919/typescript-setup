b93c6fac2294369fbd2ac34c47f13f3c808aeb56
manish typescript <tejnarayan1919@gmail.com>
2025-08-19 17:45:06 +0530

    move last k by manual process

---
+++ dist/lastKmanual.js
@@ -0,0 +1,15 @@
+"use strict";
+function lastKeleManual(arr, k) {
+    //Extract last k element
+    let n = arr.length;
+    let lastk = [];
+    for (let i = n - k; i < n; i++) {
+        lastk.push(arr[i]);
+    }
+    //extract first index to last n-k
+    for (let i = 0; i < n - k; i++) {
+        lastk.push(arr[i]);
+    }
+    return lastk;
+}
+console.log(lastKeleManual([1, 2, 3, 4, 5], 2));
