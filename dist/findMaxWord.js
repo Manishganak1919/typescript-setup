167613e6f33fe30688d7947a3a3918de96f8fb6a
manish typescript <tejnarayan1919@gmail.com>
2025-08-21 20:27:03 +0530

    find maxlength word

---
+++ dist/findMaxWord.js
@@ -0,0 +1,25 @@
+"use strict";
+function findMaxWordinSent(str) {
+    let ans = "";
+    let MaxWord = "";
+    let currLength = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === " ") {
+            if (MaxWord.length > currLength) {
+                currLength = MaxWord.length;
+                ans = MaxWord;
+            }
+            MaxWord = "";
+        }
+        else {
+            MaxWord += str[i];
+        }
+    }
+    // ✅ check last word
+    if (MaxWord.length > currLength) {
+        ans = MaxWord;
+    }
+    return ans;
+}
+const inputSentence = "I love prog in Typescript";
+console.log(findMaxWordinSent(inputSentence)); // Typescript
